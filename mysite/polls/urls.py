fdb74658e09327574a31785fe0b2c2ef5c634399
Willian <williangd@yahoo.com.br>
2015-12-24 09:58:27 -0200

    Parte 3 do tutorial

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # Ex: /polls/
     url(r'^$', views.index, name='index'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Question
+from django.template import loader
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
     #return HttpResponse("Hello, world. You're at the polls index.")
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
